eecc1ce3bf01c5c2063bf3c52b82dbfb942fab5a
Mustapha EL RHAYAMI <mustapha.elrhayami@alten.com>
2023-11-02 17:28:11 +0100

    portée

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -2,6 +2,8 @@ package com.zerofiltre.parkingbot;
 
 public class ParkingBot {
 
+  static String completion = " des équipes du parking ZeroFiltre";
+
   /**
    * Ceci est la méthode Main
    *
@@ -17,11 +19,11 @@ public class ParkingBot {
    * Cette méthode permet de dire Hello
    */
   private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue ";
+    String welcomeSentence = "Hello, recevez la bienvenue" + completion;
     System.out.println(welcomeSentence);
   }
 
   private static void sayBye() {
-    System.out.println("Recevez les aurevoirs");
+    System.out.println("Recevez les aurevoirs" + completion);
   }
 }
